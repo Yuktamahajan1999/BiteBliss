3a85ec583d9f953926a34a6018a29314d4847afd
yukta mahajan <yuktamahajan404@gmail.com>
2025-07-17 22:05:37 +0530

    Added deployed Vercel domain to CORS

--- Bliss-React-Project/src/Components/socket.js
+++ Bliss-React-Project/src/Components/socket.js
@@ -3,14 +3,17 @@ import { io } from "socket.io-client";
 let socketInstance = null;
 let connectionPromise = null;
 
+const SOCKET_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const getSocket = (token) => {
   if (!socketInstance && token) {
-    socketInstance = io("http://localhost:8000", {
+    socketInstance = io(SOCKET_URL, {
       transports: ["websocket"],
       auth: { token },
       reconnection: true,
       reconnectionAttempts: 3,
       reconnectionDelay: 1000,
+      withCredentials: true,
     });
 
     connectionPromise = new Promise((resolve) => {
